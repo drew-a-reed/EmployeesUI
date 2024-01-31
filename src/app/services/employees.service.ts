1531d286ba82e7b43055ef0a3a13c9fa275d7be5
Drew Reed <drew.reed@hotmail.com>
2024-01-30 23:55:19 -0600

    Init commit

---
+++ src/app/services/employees.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+  baseApiUrl: string = 'https://localhost:7053';
+
+  constructor(private http: HttpClient) { }
+
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.baseApiUrl + '/api/employees');
+  }
+
+  addEmployee(addEmployeeRequest: Employee): Observable<Employee>{
+    addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000';
+    return this.http.post<Employee>(this.baseApiUrl + '/api/employees', addEmployeeRequest);
+  }
+}
